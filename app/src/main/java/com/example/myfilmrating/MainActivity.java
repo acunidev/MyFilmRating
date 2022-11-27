91bcccf6b17a1cf509a517c5729f9ee3ac37186a
Alex Cuni <82770319+acunidev@users.noreply.github.com>
2022-11-27 22:10:41 +0100

    My profile saveData, getData added.

--- app/src/main/java/com/example/myfilmrating/MainActivity.java
+++ app/src/main/java/com/example/myfilmrating/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myfilmrating;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,6 +14,17 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
   }
 
+  @Override
+  protected void onPause() {
+    super.onPause();
+    Log.d("ESTATS", "Pause_Activity_1");
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    Log.d("ESTATS", "Resume_Activity_1");
+  }
 
   public void openProfile(View view) {
     Intent intent = new Intent(this, Profile.class);
@@ -24,4 +36,22 @@ public class MainActivity extends AppCompatActivity {
     startActivity(intent);
   }
 
+  @Override
+  protected void onStart() {
+    super.onStart();
+    Log.d("ESTATS", "Start_Activity_1");
+
+  }
+
+  @Override
+  protected void onStop() {
+    super.onStop();
+    Log.d("ESTATS", "Stop_Activity_1");
+  }
+
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    Log.d("ESTATS", "Destroy_Activity_1");
+  }
 }
--- app/src/main/java/com/example/myfilmrating/Profile.java
+++ app/src/main/java/com/example/myfilmrating/Profile.java
@@ -1,15 +1,120 @@
 package com.example.myfilmrating;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Profile extends AppCompatActivity {
 
+  final private static int defaultValue = 0;
+  private EditText userName;
+  private EditText email;
+  private EditText nom;
+  private EditText cognom;
+  private EditText biografia;
+  private Spinner spinner;
+  private CheckBox checkBox;
+  private int lastCheck;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.profile);
+    addItemsSpinner();
+    initElements();
+    getData();
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    saveData();
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    getData();
+  }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+    getData();
+  }
+
+  @Override
+  protected void onStop() {
+    super.onStop();
+    saveData();
+  }
+
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    saveData();
+  }
+
+  public void saveData() {
+    SharedPreferences sharedPref = getSharedPreferences("Profile", Context.MODE_PRIVATE);
+    SharedPreferences.Editor editor = sharedPref.edit();
+    editor.putString(getString(R.string.username), userName.getText().toString());
+    editor.putString(getString(R.string.email), email.getText().toString());
+    editor.putString(getString(R.string.nom), nom.getText().toString());
+    editor.putString(getString(R.string.cognom), cognom.getText().toString());
+    editor.putString(getString(R.string.biografia), biografia.getText().toString());
+    editor.putString(getString(R.string.spinGenere), (String) spinner.getSelectedItem());
+    editor.putBoolean(getString(R.string.newsletter), checkBox.isChecked());
+    editor.apply();
+  }
+
+  public void getData() {
+    SharedPreferences sharedPref = getSharedPreferences("Profile", Context.MODE_PRIVATE);
+    userName.setText(sharedPref.getString(getString(R.string.username), ""));
+    email.setText(sharedPref.getString(getString(R.string.email), ""));
+    nom.setText(sharedPref.getString(getString(R.string.nom), ""));
+    cognom.setText(sharedPref.getString(getString(R.string.cognom), ""));
+    biografia.setText(sharedPref.getString(getString(R.string.biografia), ""));
+
+    final int lengthArrayStringSpinnerGenero = getResources().getStringArray(
+        R.array.spinnerGenero).length;
+    for (int index = 0; index < lengthArrayStringSpinnerGenero; index++) {
+      final String valueSpinner = getResources().getStringArray(R.array.spinnerGenero)[index];
+      final String selectedValueSpinner = spinner.getSelectedItem().toString();
+      if (valueSpinner.equals(selectedValueSpinner)) {
+        spinner.setSelection(index);
+      }
+    }
+
+  }
+
+  public void initElements() {
+    userName.findViewById(R.id.txtUserName);
+    email.findViewById(R.id.txtEmail);
+    nom.findViewById(R.id.txtName);
+    cognom.findViewById(R.id.txtCognom);
+    biografia.findViewById(R.id.txtBio);
+    spinner.findViewById(R.id.spinnerGenero);
+    checkBox.findViewById(R.id.checkNews);
+
+
+  }
+
+  public void addItemsSpinner() {
+    Spinner spinner = (Spinner) findViewById(R.id.spinnerGenero);
+// Create an ArrayAdapter using the string array and a default spinner layout
+    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                                                                         R.array.spinnerGenero,
+                                                                         android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+    spinner.setAdapter(adapter);
   }
 }
 
